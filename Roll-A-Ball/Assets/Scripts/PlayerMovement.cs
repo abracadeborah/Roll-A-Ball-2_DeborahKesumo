282dfa8b274889cd71e12fde6d1f710a15a18327
DeborahKesumo <dkesumo@myjmc.edu.au>
2021-02-26 09:57:38 +1100

    Add base Unity project
    - Added project to GitHub
    - Added simple player movement
    - Added simple environment

---
+++ Roll-A-Ball/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float jumpForce;
+    public float moveSpeed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+        }
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+        rb.AddForce(x, 0, z);
+
+    }
+}
