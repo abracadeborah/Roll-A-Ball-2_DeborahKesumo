82ea24caeb784f77b143544df2c6669a9649877c
Deborah <dkesumo@myjmc.edu.au>
2021-04-09 12:08:32 +1000

    Created Title Screen
    - Created title screen with start and quit buttons

---
+++ Roll-A-Ball-2/Assets/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void ChangeScene(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(GetSceneName());
+    }
+
+    public void ToTitleScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public string GetSceneName()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
